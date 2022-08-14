3e4e3f047924ae8424977b105794fba3de82c1d9
ZenoHwanEth <hwan2221@hotmail.com>
2022-08-14 20:57:57 +0800

    add flow-nft-starter

---
+++ flow-nft-starter/src/cadence/scripts/getTotalSupply_script.js
@@ -0,0 +1,11 @@
+export const getTotalSupply =
+  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+  `
+import BottomShot from 0x77d3725429a2dc34;
+
+pub fun main(): UInt64 {
+
+    return BottomShot.totalSupply;
+
+}
+`;
